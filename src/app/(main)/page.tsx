a25a8cd6f4d5f82c5946ab7c273305681bdaf632
justbenuk <justbenuk@gmail.com>
2025-03-11 03:31:39 +0000

    initial commit

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,17 +2,27 @@ import PageContainer from "@/components/global/page-container";
 import SectionTitle from "@/components/global/section-title";
 import Hero from "@/components/hero/hero";
 import Stack from "@/components/stack/stack";
+import FrontContactForm from "@/forms/front-contact-form";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <PageContainer className="mt-10">
+      <PageContainer>
         <Stack />
         <SectionTitle title='Past Projects' description="Here’s a collection of everything I’ve built, some of it even works! You can find all my code on GitHub, and if you have questions, I’ll do my best to answer (or at least Google really fast)." />
         <SectionTitle title='my Blog' description="I tried my hand at blogging about web development, because why not? It might be boring, or it might be the most exciting thing you read today (unlikely, but hey, anything’s possible). Give it a scroll, you might just find something useful!" />
-        <SectionTitle title='Contact Me' description="Got a project you need help with? Hit me up! Whether it's a brand-new site or an existing one that’s throwing a tantrum, I’m here to fix, build, and make the internet a better place, one bug at a time." />
       </PageContainer>
+      <div className="mt-24" style={{ backgroundImage: `url('/assets/bg4.jpg')`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
+        <div className="h-full bg-black/50 text-white pt-8">
+          <PageContainer>
+            <SectionTitle title='Contact Me' description="Got a project you need help with? Hit me up! Whether it's a brand-new site or an existing one that’s throwing a tantrum, I’m here to fix, build, and make the internet a better place, one bug at a time." />
+            <div className="py-24 max-w-3xl mx-auto shadow-2xl">
+              <FrontContactForm />
+            </div>
+          </PageContainer>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/forms/front-contact-form.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function FrontContactForm() {
+  return (
+    <form className="bg-white rounded-2xl px-6 lg:px-12 py-12 flex flex-col space-y-12">
+      <div>
+        <Label htmlFor="name" className="text-gray-500 pb-2">Name</Label>
+        <Input id="name" placeholder="Enter Your Name" className="p-6 bg-gray-200" />
+      </div>
+      <div>
+        <Label htmlFor="email" className="text-gray-500 pb-2">Email</Label>
+        <Input id="email" placeholder="Enter Your Email" className="p-6 bg-gray-200" />
+      </div>
+      <div>
+        <Label htmlFor="message" className="text-gray-500 pb-2">Message</Label>
+        <Textarea id="message" placeholder="Whats Your Message" className="p-6 bg-gray-200 h-64" />
+      </div>
+      <div className="flex justify-center lg:justify-end">
+        <Button className="py-6 px-12 bg-green-500 uppercase font-semi">Send Message</Button>
+      </div>
+    </form>
+  )
+}
