3870e5dcf2090e0eba3c2c6018a7975e0cad1dda
Ben Andrews <justbenuk@gmail.com>
2025-10-25 06:12:42 +0100

    Initial Commit

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import { RootProps } from "@/types";
+
+export default function RootLayout({ children }: RootProps) {
+  return (
+    <div>{children}</div>
+  );
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <div>this will be the home page</div>
+  );
+}
