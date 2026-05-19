b77042a3983a1bc2c3065e3877bdfc7ae66176c7
Ben Andrews <justbenuk@gmail.com>
2026-05-19 23:29:23 +0100

    base

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import HomeBlogSection from "@/components/pages/home/HomeBlogSection";
 import HomeHeroSection from "@/components/pages/home/HomeHeroSection";
 import HomePanningSection from "@/components/pages/home/HomePanningSection";
 import MainContainer from "@/components/shared/MainContainer";
@@ -6,8 +7,9 @@ export default function Home() {
   return (
     <>
       <HomeHeroSection />
-      <MainContainer className="grid gap-20 mt-20 w-full">
+      <MainContainer className="grid gap-30 my-30 w-full">
         <HomePanningSection />
+        <HomeBlogSection />
       </MainContainer>
     </>
   );
--- src/components/pages/home/HomePanningSection.tsx
+++ src/components/pages/home/HomePanningSection.tsx
@@ -2,7 +2,7 @@ import { Code2Icon, PencilIcon, PhoneCall, ShipIcon } from "lucide-react";
 
 export default function HomePanningSection() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       <div className="flex flex-col items-center justify-center text-center gap-4 border-2 py-12 px-4 bg-secondary rounded">
         <PhoneCall className="size-8" />
         <h1 className="text-2xl font-bold text-primary">Discussion</h1>
@@ -23,7 +23,7 @@ export default function HomePanningSection() {
         <h1 className="text-2xl font-bold text-primary">Ship</h1>
         <p>Fully test and build your application. Make any changes if required</p>
       </div>
-    </div>
+    </section>
   )
 }
 
