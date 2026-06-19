8d52fc692520453fca0ca62fc0981b222b55e7d3
justbenuk <justbenuk@gmail.com>
2026-06-19 22:34:59 +0100

    base

--- src/components/MainUserMenu.tsx
+++ src/components/MainUserMenu.tsx
@@ -2,6 +2,13 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default async function MainUserMenu() {
   const session = await auth.api.getSession({
@@ -19,5 +26,24 @@ export default async function MainUserMenu() {
         </Button>
       </div>
     );
-  return <div>logged in menu</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={session.user.image as string} />
+          <AvatarFallback>BA</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem asChild>
+          <Link href={"/client"}>Client</Link>
+        </DropdownMenuItem>
+        {session.user.role === "admin" && (
+          <DropdownMenuItem asChild>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
--- src/components/shared/ThemeToggle.tsx
+++ src/components/shared/ThemeToggle.tsx
@@ -2,9 +2,19 @@
 import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 import { MoonIcon, SunIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <>
